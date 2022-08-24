88c2cb3984dc9acc711dbebdf1c9d24c92adbda7
Vishal <66051441+greenhat-cyber@users.noreply.github.com>
2022-08-23 23:24:36 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      
+
+
+      <audio
+        autoPlay="autoPlay"
+        href="audio_tag"
+        loop
+        src="assets/Music/bgm.mp3"
+        type="audio/mpeg"
+        controls
+      ></audio>
     </div>
   );
 }
