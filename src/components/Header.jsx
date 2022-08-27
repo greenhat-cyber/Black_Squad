3673d8704003c57f8d071ba88935f374a0355ec9
Vishal <66051441+greenhat-cyber@users.noreply.github.com>
2022-08-26 21:00:22 -0400

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import './Header.css'
 
 function Header() {
+
   return (
     <div>
       <div className="header">
         <div className="rt">
-
           <h1 className='fw-bolder ms-5' >Black Squad</h1>
         </div>
         <div className="lt">
@@ -42,8 +42,20 @@ function Header() {
 
       <div className="mob-nav">
         <div className="row al">
+          <div className="mob-navmenu">
 
+          </div>
           <div className="brand">
+            <i class=" fas fa-bars i-s" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop"></i>
+            <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+              <div class="offcanvas-header">
+                <h5 id="offcanvasTopLabel">Offcanvas top</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+              <div class="offcanvas-body">
+                ...
+              </div>
+            </div>
 
             <h1 className='fw-bolder' >Black Squad</h1>
           </div>
@@ -52,7 +64,11 @@ function Header() {
       </div>
 
 
+
+
+
     </div>
+
 
   )
 }
